f604580e2a94479008583eaf772ec42d152cbe21
pallav dubey <rajeevpallav606@gmail.com>
2020-05-20 16:59:05 +0530

    minor cleanups

--- app/src/main/java/com/talk2us/ui/chat/dataSource/ChatDatabase.kt
+++ app/src/main/java/com/talk2us/ui/chat/dataSource/ChatDatabase.kt
@@ -14,10 +14,6 @@ import kotlinx.coroutines.launch
 import java.sql.Time
 import java.sql.Timestamp
 
-/**
- * This is the backend. The database. This used to be done by the OpenHelper.
- * The fact that this has very few comments emphasizes its coolness.
- */
 @Database(entities = [Message::class], version = 2)
 abstract class ChatDatabase : RoomDatabase() {
 
@@ -31,21 +27,17 @@ abstract class ChatDatabase : RoomDatabase() {
             context: Context,
             scope: CoroutineScope
         ): ChatDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
+
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     ChatDatabase::class.java,
                     "messages"
                 )
-                    // Wipes and rebuilds instead of migrating if no Migration object.
-                    // Migration is not part of this codelab.
                     .fallbackToDestructiveMigration()
                     .addCallback(WordDatabaseCallback(scope))
                     .build()
                 INSTANCE = instance
-                // return instance
                 instance
             }
         }
@@ -53,14 +45,10 @@ abstract class ChatDatabase : RoomDatabase() {
         private class WordDatabaseCallback(
             private val scope: CoroutineScope
         ) : RoomDatabase.Callback() {
-            /**
-             * Override the onOpen method to populate the database.
-             * For this sample, we clear the database every time it is created or opened.
-             */
+
             override fun onOpen(db: SupportSQLiteDatabase) {
                 super.onOpen(db)
-                // If you want to keep the data through app restarts,
-                // comment out the following line.
+
                 INSTANCE?.let { database ->
                     scope.launch(Dispatchers.IO) {
                         populateDatabase(database.wordDao())
@@ -70,11 +58,8 @@ abstract class ChatDatabase : RoomDatabase() {
         }
 
         fun populateDatabase(wordDao: ChatDao) {
-            // Start the app with a clean database every time.
-            // Not needed if you only populate on creation.
+
             val word = Message("Hello", "YO",false,false,"Client")
-            wordDao.sendMessage(word)
-            wordDao.sendMessage(word)
             wordDao.sendMessage(word)
             Log.d("hello",wordDao.getAllMessages().value.toString());//Utils.toast("Message Sent")
         }
